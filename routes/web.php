a60d898495e58ee1c325e5bb170349371437abac
yasmianbenyaich <yasminabenyaich@outlouk.com>
2021-04-23 15:32:28 +0200

    products section

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('pages/about');
 });
+
+Route::get('/products', function () {
+    return view('pages/products');
+});
